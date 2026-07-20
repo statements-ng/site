cfd576fc265cf574ca32df82f5465be32bbeb572
Statements NG <statementsnigeria@gmail.com>
2026-07-20 22:22:14 +0000

    Initial app version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://statements.ng',
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['stmtng.orb.local'],
+    },
+  },
+  integrations: [sitemap()],
+});
