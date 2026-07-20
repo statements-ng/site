b6cbb3577d62b2aff5a96d51c3b0f69b50dac506
Statements NG <statementsnigeria@gmail.com>
2026-07-20 22:22:14 +0000

    ci: patch build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://statements-ng.github.io',
-  base: '/site',
+  site: 'https://statements.ng',
   vite: {
     plugins: [tailwindcss()],
     server: {
