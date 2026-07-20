c47e4c3d10aa685b33f452f607cbe8a72b20a31d
Statements NG <statementsnigeria@gmail.com>
2026-07-20 22:22:14 +0000

    ci: fix asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://statements.ng',
+  site: 'https://statements-ng.github.io',
+  base: '/site',
   vite: {
     plugins: [tailwindcss()],
     server: {
